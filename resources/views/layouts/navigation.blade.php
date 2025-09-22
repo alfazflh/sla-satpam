3d90cf2de5487edd6c83cf6c91f2084ba18eb6c3
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-09-22 13:33:44 +0700

    new commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,8 +12,10 @@
                 <!-- Navigation Links -->
                 @auth
                     <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                            {{ __('Dashboard') }}
+                        <x-nav-link :href="route('dashboard')" 
+                        :active="request()->routeIs('dashboard')" 
+                        class="text-white hover:text-[#196275]">
+                        {{ __('Dashboard') }}
                         </x-nav-link>
                     </div>
                 @endauth
