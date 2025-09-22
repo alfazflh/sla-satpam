ef85fb1b65a08828658fbd5951cbeb89ec346d6f
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-09-22 15:11:03 +0700

    new commit

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    <div class="pt-10">
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -11,5 +12,6 @@
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </x-app-layout>
