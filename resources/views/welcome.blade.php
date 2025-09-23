99dbee136fde380af6868a8aa81ea443831c1124
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-09-23 10:59:24 +0700

    new commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,6 @@
         <div class="py-12">
         <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
-                <h1 class="text-2xl font-bold mb-6 text-gray-800 dark:text-gray-100">
-                    Form Laporan Harian
-                </h1>
-
-                {{-- Komponen form --}}
                 <x-full-form />
             </div>
         </div>
