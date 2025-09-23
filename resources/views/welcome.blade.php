ae93dc343b3d7377bb4a4d3f6423c8be545c4304
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-09-23 10:45:47 +0700

    new commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    <div class="pt-10">
     <div class="py-8">
         <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
@@ -10,5 +11,6 @@
                 <x-full-form />
             </div>
         </div>
+    </div>
     </div>
 </x-app-layout>
