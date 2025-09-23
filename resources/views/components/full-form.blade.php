895cc5eb7c5bb37ccd1e158a9b24efd6b5581f43
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-09-23 10:43:43 +0700

    new commit

--- resources/views/components/full-form.blade.php
+++ resources/views/components/full-form.blade.php
@@ -1,5 +1,5 @@
 <div class="max-w-4xl mx-auto bg-white p-6 rounded-lg shadow-md">
-    <h1 class="text-xl font-bold mb-6">Form Laporan</h1>
+    <h1 class="text-xl font-bold mb-6">LAPORAN KEGIATAN ANGGOTA SATUAN PENGAMANAN</h1>
 
     @if(session('success'))
         <div class="mb-4 p-3 bg-green-100 text-green-700 rounded">{{ session('success') }}</div>
@@ -10,15 +10,15 @@
 
         {{-- Waktu, Area, Nama --}}
         <div class="mb-4">
-            <label class="block text-sm font-medium text-gray-700">Waktu</label>
+            <label class="block text-md font-medium text-gray-700">1. Waktu Jaga Shift</label>
             <input type="datetime-local" name="waktu" class="mt-1 w-full rounded-md border-gray-300 shadow-sm">
         </div>
         <div class="mb-4">
-            <label class="block text-sm font-medium text-gray-700">Area</label>
+            <label class="block text-sm font-medium text-gray-700">2. Area kerja</label>
             <input type="text" name="area" class="mt-1 w-full rounded-md border-gray-300 shadow-sm">
         </div>
         <div class="mb-4">
-            <label class="block text-sm font-medium text-gray-700">Nama</label>
+            <label class="block text-sm font-medium text-gray-700">3. Nama Petugas Jaga</label>
             <input type="text" name="nama" class="mt-1 w-full rounded-md border-gray-300 shadow-sm">
         </div>
 
