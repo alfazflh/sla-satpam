94b250531f40c6c9501394e256440b943bd7e649
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-09-23 10:55:37 +0700

    new commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <div class="pt-15">
-    <div class="py-8">
+    <div class="pt-10"> {{-- padding top biar konten nggak ketiban navbar --}}
+        <div class="py-12">
         <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
                 <h1 class="text-2xl font-bold mb-6 text-gray-800 dark:text-gray-100">
