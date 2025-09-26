50277d542e46a233ac62162b5fd2b0d6aa6ea9b6
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-09-26 14:29:34 +0700

    new commit

--- resources/views/components/full-form.blade.php
+++ resources/views/components/full-form.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laporan Kegiatan Anggota Satuan Pengamana</title>
+    <title>Laporan Kegiatan Anggota Satuan Pengamanan</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         .step-indicator {
