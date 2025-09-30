0d922752e730c8a1a14f1ca63dd6a8a20e0cddd7
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-09-30 08:09:27 +0700

    new commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
     <div class="pt-10"> {{-- padding top biar konten nggak ketiban navbar --}}
         <div class="py-12">
         <div class="max-w-5xl mx-auto sm:px-6 lg:px-8" >
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6" style="background-color: #1f7389;">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6" style="background-color: #F3F4F6;">
                 <x-full-form />
             </div>
     </div>
