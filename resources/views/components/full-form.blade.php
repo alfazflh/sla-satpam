36256fe667aaea26ce4880a72afa56f0b919e298
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-10-01 08:50:56 +0700

    new commit

--- resources/views/components/full-form.blade.php
+++ resources/views/components/full-form.blade.php
@@ -1303,7 +1303,7 @@
                 Swal.fire({
                     icon: 'warning',
                     title: 'Perhatian!',
-                    text: 'Silakan pilih salah satu opsi',
+                    text: 'Silakan pilih salah satu jawaban',
                     confirmButtonColor: '#1f7389'
                 });
                 return;
