c5e922eedee0e2a4483b2700f4fe87eec4f568c8
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-10-06 10:24:57 +0700

    new commit

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,13 +1,10 @@
 <x-app-layout>
     <div class="pt-10"> {{-- padding top biar konten nggak ketiban navbar --}}
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">
-                        {{ __("You're logged in as user!") }}
-                    </div>
-                </div>
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8" >
+            <div class="bg-white overflow-hidden sm:rounded-lg p-6" style="background-color: #F3F4F6;">
+                <x-full-form />
             </div>
-        </div>
+    </div>
     </div>
 </x-app-layout>
