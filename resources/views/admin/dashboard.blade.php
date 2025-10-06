c08996a6ceb0c9c68c532db3d7affa982ac5a78d
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-10-06 09:53:04 +0700

    new commit

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -404,7 +404,7 @@
                     <svg class="w-5 h-5 text-red-500 mr-2 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clip-rule="evenodd"/>
                     </svg>
-                    <span class="text-gray-700 text-sm">${filename}</span>
+                    <span class="text-gray-700 text-sm truncate block max-w-full">${filename}</span>
                 `;
                 
                 gallery.appendChild(photoItem);
