6fdd602b3ffd05ab048161d692b720871c71ff21
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-10-06 09:31:58 +0700

    new commit

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -143,14 +143,6 @@
                                 </button>
                                 <p id="remainingCount" class="text-sm text-gray-500 mt-2"></p>
                             </div>
-
-                            <!-- Link Lihat Folder -->
-                            <div class="mt-4 flex items-center text-blue-600 hover:text-blue-700">
-                                <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"/>
-                                </svg>
-                                <a href="#" class="font-medium">Lihat folder</a>
-                            </div>
                         </div>
                     </div>
     
