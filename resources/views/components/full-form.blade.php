5876b79dbab4ebbbda431b9319a8506662e7eab2
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-10-12 14:17:34 +0000

    12 Oct 2025, 21:17

--- resources/views/components/full-form.blade.php
+++ resources/views/components/full-form.blade.php
@@ -165,7 +165,7 @@
                                 focus:ring-offset-1 cursor-pointer transition">
                             📷 Upload Foto
                         </button>
-                        <input type="file" name="foto_serahterima" accept="image/*" style="display:none">
+                        <input type="file" name="foto_serahterima[]" accept="image/*" multiple style="display:none">
                         <span class="fileName block mt-2 text-sm text-gray-600 italic"></span>
                     </div>
 
