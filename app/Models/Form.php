a520b09bf211bc0ff0afa23bca2a6aabdda522c1
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-10-15 03:41:37 +0000

    15 Oct 2025, 10:41

--- app/Models/Form.php
+++ app/Models/Form.php
@@ -1,35 +1,90 @@
 <?php
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Form extends Model
 {
+    use HasFactory;
+
+    protected $table = 'laporan_pengamanan';
+
     protected $fillable = [
-        'waktu', 'area', 'nama',
-        'foto_serahterima', 'foto_patroli', 'foto_lembur', 'foto_tamu',
-        'foto_panduan', 'foto_force', 'foto_penertiban', 'foto_simulasi',
-        'foto_penyegaran', 'foto_telepon', 'foto_rutin', 'foto_pengecekan',
-        'foto_cctv', 'ketentuan_seragam', 'pengamanan', 'kronologi_kriminal',
-        'fungsi_khusus', 'kronologi_gangguan', 'memantau', 'pelayanan',
-        'fungsi_force', 'penertiban', 'simulasi', 'penyegaran', 'telepon',
-        'titik', 'rutin', 'pengecekan', 'cctv', 'kronologi_cctv'
+        'waktu', 'area', 'nama', 'ketentuan_seragam', 'pengamanan', 
+        'kronologi_kriminal', 'fungsi_khusus', 'kronologi_gangguan', 
+        'memantau', 'pelayanan', 'fungsi_force', 'penertiban', 
+        'simulasi', 'penyegaran', 'telepon', 'rutin', 'titik', 
+        'pengecekan', 'cctv', 'kronologi_cctv',
+        'foto_serahterima', 'foto_patroli', 'foto_lembur', 'foto_tamu', 
+        'foto_panduan', 'foto_force', 'foto_penertiban', 'foto_simulasi', 
+        'foto_penyegaran', 'foto_telepon', 'foto_rutin', 'foto_pengecekan', 
+        'foto_cctv'
     ];
 
-    protected $casts = [
-        'nama' => 'array',
-        'foto_serahterima' => 'array',
-        'foto_patroli' => 'array',
-        'foto_lembur' => 'array',
-        'foto_tamu' => 'array',
-        'foto_panduan' => 'array',
-        'foto_force' => 'array',
-        'foto_penertiban' => 'array',
-        'foto_simulasi' => 'array',
-        'foto_penyegaran' => 'array',
-        'foto_telepon' => 'array',
-        'foto_rutin' => 'array',
-        'foto_pengecekan' => 'array',
-        'foto_cctv' => 'array',
-    ];
+    // Accessor untuk foto - otomatis convert JSON ke array saat diambil
+    protected function getFotoSerahterimarAttribute($value)
+    {
+        return $value ? json_decode($value, true) : [];
+    }
+
+    protected function getFotoPatroliAttribute($value)
+    {
+        return $value ? json_decode($value, true) : [];
+    }
+
+    protected function getFotoLemburAttribute($value)
+    {
+        return $value ? json_decode($value, true) : [];
+    }
+
+    protected function getFotoTamuAttribute($value)
+    {
+        return $value ? json_decode($value, true) : [];
+    }
+
+    protected function getFotoPanduanAttribute($value)
+    {
+        return $value ? json_decode($value, true) : [];
+    }
+
+    protected function getFotoForceAttribute($value)
+    {
+        return $value ? json_decode($value, true) : [];
+    }
+
+    protected function getFotoPenertibanAttribute($value)
+    {
+        return $value ? json_decode($value, true) : [];
+    }
+
+    protected function getFotoSimulasiAttribute($value)
+    {
+        return $value ? json_decode($value, true) : [];
+    }
+
+    protected function getFotoPenyegaranAttribute($value)
+    {
+        return $value ? json_decode($value, true) : [];
+    }
+
+    protected function getFotoTeleponAttribute($value)
+    {
+        return $value ? json_decode($value, true) : [];
+    }
+
+    protected function getFotoRutinAttribute($value)
+    {
+        return $value ? json_decode($value, true) : [];
+    }
+
+    protected function getFotoPengecekanAttribute($value)
+    {
+        return $value ? json_decode($value, true) : [];
+    }
+
+    protected function getFotoCctvAttribute($value)
+    {
+        return $value ? json_decode($value, true) : [];
+    }
 }
